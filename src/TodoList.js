221583f22a8bfefd3308a5516bdc175ce97c00d8
shuaipengge <shuaipengge@gmail.com>
2020-01-10 23:15:13 +0800

    数据绑定

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,20 +1,36 @@
-import React, { Fragment } from "react";
+import React, { Component, Fragment } from "react";
 // Fragment 占位符 隐藏最外层标签
-
-function TodoList() {
-  // JSX
-  return (
-    <Fragment>
-      <div>
-        <input></input>
-        <button>提交</button>
-      </div>
-      <ul>
-        <li>学react</li>
-        <li>学vue</li>
-      </ul>
-    </Fragment>
-  );
+class TodoList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      inputValue: "",
+      list: []
+    };
+  }
+  render() {
+    return (
+      <Fragment>
+        <div>
+          <input
+            value={this.state.inputValue}
+            onChange={this.handleInputChange.bind(this)}
+          />
+          <button>提交</button>
+        </div>
+        <ul>
+          <li>学react</li>
+          <li>学vue</li>
+        </ul>
+      </Fragment>
+    );
+  }
+  handleInputChange(e) {
+    this.setState({
+      inputValue: e.target.value
+    });
+    console.log(e.target.value);
+  }
 }
 
 export default TodoList;
