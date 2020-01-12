2f3d2fc7acc2215d2e63ca3d672290dded108e93
shuaipengge <shuaipengge@gmail.com>
2020-01-12 19:13:38 +0800

    修改为无状态组件

--- src/TodoListUI.js
+++ src/TodoListUI.js
@@ -2,43 +2,41 @@ import React, { Component } from "react";
 import { Input, Button, List } from "antd";
 import "antd/dist/antd.css";
 
-class TodoListUI extends Component {
-  render() {
-    return (
-      <div style={{ marginTop: "10px", marginLeft: "10px" }}>
-        <div>
-          <Input
-            value={this.props.inputValue}
-            placeholder="Todo info"
-            style={{ width: "300px", marginRight: "10px" }}
-            onChange={this.props.handleInputChange}
-          />
-          <Button type="primary" onClick={this.props.handleBtnClick}>
-            提交
-          </Button>
-        </div>
-        <List
-          style={{ marginTop: "10px", width: "300px" }}
-          bordered
-          dataSource={this.props.list}
-          renderItem={(item, index) => (
-            <List.Item>
-              {item}
-              <Button
-                onClick={()=>{
-                  this.props.handleItemDelete(index)
-                }}
-                type="danger"
-                shape="circle"
-                icon="delete"
-                style={{ marginLeft: "10px" }}
-              />
-            </List.Item>
-          )}
+const TodoListUI = props => {
+  return (
+    <div style={{ marginTop: "10px", marginLeft: "10px" }}>
+      <div>
+        <Input
+          value={props.inputValue}
+          placeholder="Todo info"
+          style={{ width: "300px", marginRight: "10px" }}
+          onChange={props.handleInputChange}
         />
+        <Button type="primary" onClick={props.handleBtnClick}>
+          提交
+        </Button>
       </div>
-    );
-  }
-}
+      <List
+        style={{ marginTop: "10px", width: "300px" }}
+        bordered
+        dataSource={props.list}
+        renderItem={(item, index) => (
+          <List.Item>
+            {item}
+            <Button
+              onClick={() => {
+                props.handleItemDelete(index);
+              }}
+              type="danger"
+              shape="circle"
+              icon="delete"
+              style={{ marginLeft: "10px" }}
+            />
+          </List.Item>
+        )}
+      />
+    </div>
+  );
+};
 
-export default TodoListUI
+export default TodoListUI;
