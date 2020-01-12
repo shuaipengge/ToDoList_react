2e0a39e6b313fc8dc0ce02bc0932f901c6111e43
shuaipengge <shuaipengge@gmail.com>
2020-01-12 14:40:39 +0800

    使用ant.design框架

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,94 +1,34 @@
-import React, { Component, Fragment } from "react";
-import TodoItem from "./TodoItem";
-import axios from "axios";
-import "./style.css";
+import React, { Component } from "react";
+import { Input, Button, List } from "antd";
+import "antd/dist/antd.css";
 
-// Fragment 占位符 隐藏最外层标签
+const data = [
+  "Racing car sprays burning fuel into crowd.",
+  "Japanese princess to wed commoner.",
+  "Australian walks 100km after outback crash.",
+  "Man charged over missing wedding girl.",
+  "Los Angeles battles huge wildfires."
+];
+
 class TodoList extends Component {
-  constructor(props) {
-    super(props);
-    // 当组件的state 或者props发生改变的时候 render函数就会重新执行
-    this.state = {
-      inputValue: "",
-      list: ["学习Vue", "学习React"]
-    };
-    this.handleInputChange = this.handleInputChange.bind(this);
-    this.handleBtnClick = this.handleBtnClick.bind(this);
-    this.handleItemDelete = this.handleItemDelete.bind(this);
-  }
   render() {
-    console.log("render");
     return (
-      <Fragment>
-        {/* 下面是组件内容 */}
+      <div style={{ marginTop: "10px", marginLeft: "10px" }}>
         <div>
-          <label htmlFor="insertArea">输入内容</label>
-          <input
-            id="insertArea"
-            className="input"
-            value={this.state.inputValue}
-            onChange={this.handleInputChange}
-            ref={input => {
-              this.input = input;
-            }}
+          <Input
+            placeholder="Todo info"
+            style={{ width: "300px", marginRight: "10px" }}
           />
-          <button onClick={this.handleBtnClick}>提交</button>
+          <Button type="primary">提交</Button>
         </div>
-        <ul>{this.getTodoItem()}</ul>
-      </Fragment>
+        <List
+          style={{ marginTop: "10px", width: "300px" }}
+          bordered
+          dataSource={data}
+          renderItem={item => <List.Item>{item}</List.Item>}
+        />
+      </div>
     );
-  }
-
-  componentDidMount() {
-    axios
-      .get("/api/todolist.json")
-      .then(res => {
-        console.log(res.data);
-        this.setState(() => ({ list: [...res.data] }));
-      })
-      .catch(() => {
-        alert("error");
-      });
-  }
-
-  getTodoItem() {
-    return this.state.list.map((item, index) => {
-      return (
-        <div key={index}>
-          {/* 父组件向子组件传值 属性方式 */}
-          <TodoItem
-            content={item}
-            index={index}
-            deleteItem={this.handleItemDelete}
-          />
-        </div>
-      );
-    });
-  }
-
-  handleInputChange() {
-    const value = this.input.value;
-    this.setState(() => ({
-      inputValue: value
-    }));
-  }
-
-  handleBtnClick() {
-    this.setState(prevState => ({
-      list: [...prevState.list, prevState.inputValue],
-      inputValue: ""
-    }));
-  }
-
-  handleItemDelete(index) {
-    // immutable
-    // state 不允许我们做任何改变
-
-    this.setState(prevState => {
-      const list = [...prevState.list];
-      list.splice(index, 1);
-      return { list };
-    });
   }
 }
 
