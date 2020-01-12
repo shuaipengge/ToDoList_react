b789b836f0e6a779114e6a5d5d4cecdd5b33191e
shuaipengge <shuaipengge@gmail.com>
2020-01-12 21:12:34 +0800

    Redux中发送异步请求获取数据

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,8 +1,9 @@
 import React, { Component } from "react";
 
 import store from "./store";
-import { getInputChangeAction, getAddItemAction, getDeleteItemAction } from './store/actionCreators'
+import { getInputChangeAction, getAddItemAction, getDeleteItemAction, initListAction } from './store/actionCreators'
 import TodoListUI from './TodoListUI'
+import axios from 'axios'
 
 
 class TodoList extends Component {
@@ -26,6 +27,15 @@ class TodoList extends Component {
         handleItemDelete={this.handleItemDelete}
       />
     )
+  }
+
+  componentDidMount() {
+    axios.get('api/todolist.json').then((res) => {
+      const data = res.data
+      const action = initListAction(data)
+      store.dispatch(action)
+      console.log(action);
+    })
   }
 
   handleInputChange(e) {
